d39fe165b1775c3fa2cd4204a072c6235dea71a6
Rovaron-Dev <rovaron.rodrigo@gmail.com>
2026-08-25 23:31:49 -0300

    comitado

--- Projeto da feira/Projeto da feira/Form1.Designer.cs
+++ Projeto da feira/Projeto da feira/Form1.Designer.cs
@@ -29,13 +29,165 @@ namespace Projeto_da_feira
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.FplaylistCard2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.FplaylistCard4 = new Guna.UI2.WinForms.Guna2Panel();
+            this.FplaylistCard3 = new Guna.UI2.WinForms.Guna2Panel();
+            this.Fpanel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.FplaylistCard1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.XcardMusic = new Guna.UI2.WinForms.Guna2Panel();
+            this.XcardMusic2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.XcardMusic3 = new Guna.UI2.WinForms.Guna2Panel();
+            this.XcardMusic4 = new Guna.UI2.WinForms.Guna2Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 19.99009F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.65172F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 42.35819F));
+            this.tableLayoutPanel1.Controls.Add(this.FplaylistCard2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.FplaylistCard4, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.FplaylistCard3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Fpanel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.FplaylistCard1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(995, 553);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // FplaylistCard2
+            // 
+            this.FplaylistCard2.Location = new System.Drawing.Point(200, 71);
+            this.FplaylistCard2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.FplaylistCard2.Name = "FplaylistCard2";
+            this.FplaylistCard2.Size = new System.Drawing.Size(150, 61);
+            this.FplaylistCard2.TabIndex = 1;
+            this.FplaylistCard2.Paint += new System.Windows.Forms.PaintEventHandler(this.guna2Panel1_Paint);
+            // 
+            // FplaylistCard4
+            // 
+            this.FplaylistCard4.Location = new System.Drawing.Point(574, 71);
+            this.FplaylistCard4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.FplaylistCard4.Name = "FplaylistCard4";
+            this.FplaylistCard4.Size = new System.Drawing.Size(150, 57);
+            this.FplaylistCard4.TabIndex = 2;
+            // 
+            // FplaylistCard3
+            // 
+            this.FplaylistCard3.Location = new System.Drawing.Point(574, 2);
+            this.FplaylistCard3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.FplaylistCard3.Name = "FplaylistCard3";
+            this.FplaylistCard3.Size = new System.Drawing.Size(150, 64);
+            this.FplaylistCard3.TabIndex = 2;
+            this.FplaylistCard3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint_1);
+            // 
+            // Fpanel1
+            // 
+            this.Fpanel1.Location = new System.Drawing.Point(2, 2);
+            this.Fpanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Fpanel1.Name = "Fpanel1";
+            this.Fpanel1.Size = new System.Drawing.Size(150, 64);
+            this.Fpanel1.TabIndex = 1;
+            // 
+            // FplaylistCard1
+            // 
+            this.FplaylistCard1.Location = new System.Drawing.Point(200, 2);
+            this.FplaylistCard1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.FplaylistCard1.Name = "FplaylistCard1";
+            this.FplaylistCard1.Size = new System.Drawing.Size(150, 64);
+            this.FplaylistCard1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 2);
+            this.flowLayoutPanel1.Controls.Add(this.XcardMusic);
+            this.flowLayoutPanel1.Controls.Add(this.XcardMusic2);
+            this.flowLayoutPanel1.Controls.Add(this.XcardMusic3);
+            this.flowLayoutPanel1.Controls.Add(this.XcardMusic4);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(200, 140);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(793, 411);
+            this.flowLayoutPanel1.TabIndex = 3;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // XcardMusic
+            // 
+            this.XcardMusic.Location = new System.Drawing.Point(2, 2);
+            this.XcardMusic.Margin = new System.Windows.Forms.Padding(2);
+            this.XcardMusic.Name = "XcardMusic";
+            this.XcardMusic.Size = new System.Drawing.Size(150, 61);
+            this.XcardMusic.TabIndex = 2;
+            // 
+            // XcardMusic2
+            // 
+            this.XcardMusic2.Location = new System.Drawing.Point(156, 2);
+            this.XcardMusic2.Margin = new System.Windows.Forms.Padding(2);
+            this.XcardMusic2.Name = "XcardMusic2";
+            this.XcardMusic2.Size = new System.Drawing.Size(150, 61);
+            this.XcardMusic2.TabIndex = 3;
+            // 
+            // XcardMusic3
+            // 
+            this.XcardMusic3.Location = new System.Drawing.Point(310, 2);
+            this.XcardMusic3.Margin = new System.Windows.Forms.Padding(2);
+            this.XcardMusic3.Name = "XcardMusic3";
+            this.XcardMusic3.Size = new System.Drawing.Size(150, 61);
+            this.XcardMusic3.TabIndex = 3;
+            // 
+            // XcardMusic4
+            // 
+            this.XcardMusic4.Location = new System.Drawing.Point(464, 2);
+            this.XcardMusic4.Margin = new System.Windows.Forms.Padding(2);
+            this.XcardMusic4.Name = "XcardMusic4";
+            this.XcardMusic4.Size = new System.Drawing.Size(150, 61);
+            this.XcardMusic4.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(990, 550);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private Guna.UI2.WinForms.Guna2Panel FplaylistCard3;
+        private Guna.UI2.WinForms.Guna2Panel Fpanel1;
+        private Guna.UI2.WinForms.Guna2Panel FplaylistCard1;
+        private Guna.UI2.WinForms.Guna2Panel FplaylistCard4;
+        private Guna.UI2.WinForms.Guna2Panel FplaylistCard2;
+        private Guna.UI2.WinForms.Guna2Panel XcardMusic;
+        private Guna.UI2.WinForms.Guna2Panel XcardMusic2;
+        private Guna.UI2.WinForms.Guna2Panel XcardMusic3;
+        private Guna.UI2.WinForms.Guna2Panel XcardMusic4;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
 
--- Projeto da feira/Projeto da feira/Form1.cs
+++ Projeto da feira/Projeto da feira/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Guna.UI2.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,123 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Net;
+using System.IO;
 
 namespace Projeto_da_feira
 {
     public partial class Form1 : Form
+
     {
+        public static class Cores
+        {
+            public static Color Fundo = Color.FromArgb(255, 5, 5, 7);
+            public static Color FundoSecundario = Color.FromArgb(255, 19, 18, 26);
+            public static Color Roxo = Color.FromArgb(255, 101, 95, 188);
+
+
+        }
         public Form1()
         {
             InitializeComponent();
+            startGraph();
+        }
+        private void Card(Control container)
+        {
+            foreach (Control controle in container.Controls)
+            {
+                
+                if (controle is Guna.UI2.WinForms.Guna2Panel panel &&
+                    panel.Name.StartsWith("F"))
+                {
+                    panel.Dock = DockStyle.Fill;
+                    panel.Margin = new Padding(10);
+
+                    panel.FillColor = Cores.FundoSecundario;
+
+
+                    panel.BorderRadius = 10;
+                    panel.BorderThickness = 0;
+                }else if(controle is Guna.UI2.WinForms.Guna2Panel panel2 &&
+                    panel2.Name.StartsWith("X"))
+                {
+                    panel2.Dock = DockStyle.None;
+                    panel2.Width = 150;
+                    panel2.Height = 180;
+                    panel2.Margin = new Padding(10);
+
+                    panel2.FillColor = Cores.FundoSecundario;
+
+
+                    panel2.BorderRadius = 10;
+                    panel2.BorderThickness = 0;
+                }
+                if (controle.HasChildren)
+                {
+                    Card(controle);
+                }
+            }
+
+            
+            
+        }
+        private void CarregarImagem(PictureBox pictureBox, string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                byte[] dados = client.DownloadData(url);
+
+                using (MemoryStream stream = new MemoryStream(dados))
+                {
+                    pictureBox.Image = Image.FromStream(stream);
+                }
+            }
+        }
+        private void startGraph()
+        {
+
+
+
+            Card(this);
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.SetRowSpan(Fpanel1, 2);
+
+
+        }
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2CustomGradientPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void guna2CustomGradientPanel1_Paint_1(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void guna2Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
